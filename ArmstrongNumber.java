d29d3c636983627a23d3461404b57e81c559160b
DhairyaSinghHariom <sdhairya192@gmail.com>
2025-12-21 21:45:55 +0530

    Implement Armstrong number checker in Java

---
+++ ArmstrongNumber.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int sum = 0;
+        int digits = 0;
+        int temp = num;
+
+        // Count number of digits
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        temp = num;
+
+        // Calculate Armstrong sum
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += Math.pow(digit, digits);
+            temp /= 10;
+        }
+
+        // Check result
+        if (sum == original) {
+            System.out.println(original + " is an Armstrong number.");
+        } else {
+            System.out.println(original + " is not an Armstrong number.");
+        }
+    }
+}
